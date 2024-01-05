d95abe6ee388e11e2a8d524a9657cc64788f3363
Nur E Azam (Nur) <91000171+nureazambd@users.noreply.github.com>
2024-01-05 09:54:15 +0000

    JavaScript conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+let userId = 12345;
+let newUserId = String(userId);
+let addUser = userId + newUserId;
+let addUser2 = userId + 6;
+
+let score = 'nur'
+let valueInNumber = Number(score);
+
+let intNumber = "45" + 5
+
+let bol = 444
+
+let value = 5;
+let negValue = -value;
+
+console.log(4 + 6);
+console.log(10 - 5);
+console.log(5*5)
+console.log(5**2);
+console.log(10 / 2);
+console.log(11 % 2);
+
+let str1 = 'Nur-'
+let str2 = 'E'
+let str3 = '-Azam'
+let fullName = str1+str2+str3;
+console.log(fullName);
+console.log(5 + "3");
+console.log("5" + 4);
+
+console.log(+true);
+console.log(+"");
+
+console.log(true);
+console.log("");
+let a_number = 100;
+++a_number;
+console.log(a_number)
+
+
+
+// console.log(value);
+// console.log(negValue);
+// console.log( Boolean(bol))
+
+// console.log(typeof intNumber);
+// console.log(intNumber);
+
+// console.log(typeof(score));
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// console.log(typeof userId);
+// console.log(typeof newUserId);
+// console.log(typeof addUser);
+// console.log(typeof addUser2);
+
+
