a9ee60d46d7c77ea114bb988ef65b48370629f92
Nur E Azam (Nur) <91000171+nureazambd@users.noreply.github.com>
2024-01-06 13:42:20 +0000

    JavaScript string all function

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = 'nureazam';
+const repoCount = 50;
+// console.log(name + repoCount + " value")
+// console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('   nureazam-CSE-nsu   ')
+//console.log(gameName);
+//console.log(gameName.charAt(2)); //(method) String.charAt(pos: number): string Returns the character at the specified index.
+//console.log(gameName.indexOf('a')); //Returns the position of the first occurrence of a substring.
+//console.log(gameName.lastIndexOf('a')) //Returns the last occurrence of a substring in the string.
+//console.log(gameName.length) //Returns the length of a String object.
+// console.log(gameName.localeCompare("m"));
+// console.log(gameName.match('a')); //Matches a string with a regular expression, and returns an array containing the results of that search.
+// console.log(gameName.replace('a', 'q')); //Replaces text in a string, using a regular expression or search string.
+// console.log(gameName.replaceAll('a', 'q')); //Replaces all text in a string, using a regular expression or search string.
+// console.log(gameName.repeat(2)); //Returns a String value that is made from count copies appended together. If count is 0, the empty string is returned.
+// console.log(gameName.search('a')); //Finds the first substring match in a regular expression search.
+// console.log(gameName.slice(0,2)); //Returns a section of a string.
+// console.log(gameName.split('z')); //Split a string into substrings using the specified separator and return them as an array.
+// console.log(gameName.startsWith('a',4)) //Returns true if the sequence of elements of searchString converted to a String is the same as the corresponding elements of this object (converted to a String) starting at position. Otherwise returns false.
+// console.log(gameName.substring(3,8)); //Returns the substring at the specified location within a String object.
+// console.log(gameName.toLocaleLowerCase()); //Converts all alphabetic characters to lowercase, taking into account the host environment's current locale.
+// console.log(gameName.toLocaleUpperCase()); //Returns a string where all alphabetic characters have been converted to uppercase, taking into account the host environment's current locale.
+// console.log(gameName.toLowerCase());//Converts all the alphabetic characters in a string to lowercase.
+// console.log(gameName.toUpperCase());//Converts all the alphabetic characters in a string to uppercase.
+// console.log(gameName.toString());//Returns a string representation of a string.
+// console.log(gameName.trim());//Removes the leading and trailing white space and line terminator characters from a string.
+// console.log(gameName.trimEnd()); //Removes the trailing white space and line terminator characters from a string.
+// console.log(gameName.trimStart()); //Removes the leading white space and line terminator characters from a string.
+// console.log(gameName.valueOf()) //Returns the primitive value of the specified object.
